798dbfd83ce6ab47d38928960f8956af9f20cfca
Melisa Luciano <42757703+MelisaLuciano@users.noreply.github.com>
2019-12-05 11:50:14 -0600

    8 problem

---
+++ 8WAD/src/java/paquete/Servlet1.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class Servlet1 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            PrintWriter out = response.getWriter();
+            String nombre = request.getParameter("parametro1");
+            RequestDispatcher rd=request.getRequestDispatcher("Servlet2");
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Servlet1</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Hola</h1>");
+            out.println("</body>");
+            out.println("</html>");
+            request.setAttribute("atributorequest", "waaaa que loco");
+            rd.forward(request, response);
+
+    }
+
+}
