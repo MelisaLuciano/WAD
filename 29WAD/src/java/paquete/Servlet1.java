6c33d0db10015d5c2d079ba2ab94a8bbe6e63e66
Melisa Luciano <42757703+MelisaLuciano@users.noreply.github.com>
2019-12-05 18:21:24 -0600

    29 problem

---
+++ 29WAD/src/java/paquete/Servlet1.java
@@ -0,0 +1,40 @@
+package paquete;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class Servlet1 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        
+        response.setContentType("text/html;charset=UTF-8");
+        Enumeration enumera = request.getHeaderNames();
+        String hostremoto=request.getRemoteHost();
+                
+        PrintWriter out = response.getWriter();
+        
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Servlet1</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            //out.println("<h1>"getQueryString:"+cadena+"</h1>");
+            while(enumera.hasMoreElements())
+            {
+              String encabezado = (String) enumera.nextElement();
+              out.println("encabezado (" + encabezado + ") " );
+            }
+            out.println("</body>");
+            out.println("</html>");
+            response.setHeader("Refresh", "3");
+    }
+
+}
